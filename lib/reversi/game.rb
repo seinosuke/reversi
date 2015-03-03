6b9a48e6dac0a4a97455f595b6e3e500e31bb09d
seinosuke <seinosuke.3606@gmail.com>
2015-03-03 16:15:30 +0900

    Game#resetをプライベートに変更

--- lib/reversi/game.rb
+++ lib/reversi/game.rb
@@ -5,6 +5,32 @@ module Reversi
 
     # Initializes a new Board object.
     def initialize(options = {})
+      reset(options)
+    end
+
+    # Set the option values and start a game.
+    #
+    # @see #run
+    def start(options = {})
+      reset(options)
+      mode = {:progress => @progress, :vs_human => @vs_human}
+      run(mode)
+    end
+
+    private
+
+    # Reset the instance variable, `@board`.
+    def reset(options = {})
+      load_config(options)
+      @board = Board.new(@options)
+      @player_class_b = @player_b
+      @player_class_w = @player_w
+      @player_b = @player_class_b.new(:black, @board)
+      @player_w = @player_class_w.new(:white, @board)
+    end
+
+    # Load the configuration.
+    def load_config(options = {})
       Reversi.set_defaults
       @options = Reversi.options.merge(options)
       Configuration::OPTIONS_KEYS.each do |key|
@@ -16,48 +42,25 @@ module Reversi
         @progress = false
       end
       @vs_human ||= false
-
-      @board = Board.new(@options)
-      @player_class_b = @player_b
-      @player_class_w = @player_w
-      @player_b = @player_class_b.new(:black, @board)
-      @player_w = @player_class_w.new(:white, @board)
     end
-
-    # Set the option values and start a game.
-    #
-    # @see #run
-    def start
-      options = {:progress => @progress, :vs_human => @vs_human}
-      run(options)
-    end
-
-    # Reset the instance variable, `@board`.
-    def reset
-      @board = Board.new(@options)
-      @player_b = @player_class_b.new(:black, @board)
-      @player_w = @player_class_w.new(:white, @board)
-    end
-
-    private
 
     # Execute a game and run until the end of the game.
     #
-    # @option options [Boolean] :progress Display the progress of the game.
-    # @option options [Boolean] :vs_human 
-    def run(options = {})
-      show_board if options[:progress]
+    # @option mode [Boolean] :progress Display the progress of the game.
+    # @option mode [Boolean] :vs_human 
+    def run(mode = {})
+      show_board if mode[:progress]
       loop do
         break if game_over?
         @status = @board.status
         @player_b.move(@board); check_move(:black)
-        show_board if options[:progress]
+        show_board if mode[:progress]
 
         @status = @board.status
         @player_w.move(@board); check_move(:white)
-        show_board if options[:progress]
+        show_board if mode[:progress]
       end
-      puts @board.to_s if options[:progress] || options[:vs_human]
+      puts @board.to_s if mode[:progress] || mode[:vs_human]
     end
 
     # Show the current state of this game board.
@@ -68,18 +71,20 @@ module Reversi
 
     # Checks a move to make sure it is valid.
     def check_move(color)
-      blank_diff = @status[:none] - @board.count_disks(:none)
+      blank_diff = @status[:none].size - @board.count_disks(:none)
+
       case blank_diff
       when 1
-        if (@board.count_disks(color) - @status[color]) < 2
+        if (@board.count_disks(color) - @status[color].size) < 2
           raise MoveError, "A player must flip at least one or more opponent's disks."
         end
       when 0
-        unless (@board.count_disks(:black) - @status[:black]) == 0 &&
-               (@board.count_disks(:white) - @status[:white]) == 0
+        unless (@board.count_disks(:black) - @status[:black].size) == 0 &&
+               (@board.count_disks(:white) - @status[:white].size) == 0
           raise MoveError, "When a player can't make a valid move, you must not place a new disk."
         end
-      else raise MoveError, "A player must place a new disk on the board."
+      else
+        raise MoveError, "A player must place a new disk on the board."
       end
     end
 
