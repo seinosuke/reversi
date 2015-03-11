1baeb43ba7bba87c83d879c7dbde0c9379c46374
seinosuke <seinosuke.3606@gmail.com>
2015-03-11 19:57:40 +0900

    MinMaxAIを追加

--- lib/reversi/player.rb
+++ lib/reversi/player.rb
@@ -2,6 +2,7 @@ module Reversi
   module Player
     autoload :RandomAI, "reversi/player/random_ai"
     autoload :NegaMaxAI, "reversi/player/nega_max_ai"
+    autoload :MinMaxAI, "reversi/player/min_max_ai"
     autoload :Human, "reversi/player/human"
   end
 end
